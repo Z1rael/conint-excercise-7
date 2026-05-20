f0f50aecabc53014231c46b982f3042d629c7752
Felix Dilly <felix.dilly@gmail.com>
2026-05-20 08:36:53 +0200

    feat: added jest dependecies

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    // Tell Jest to use ts-jest for .ts/.tsx files
+    preset: 'ts-jest',
+
+    // Use Node as the test environment
+    testEnvironment: 'node',
+
+    // Make sure Jest knows to look for .ts files
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    testPathIgnorePatterns: [
+        "<rootDir>/dist/",
+        "<rootDir>/node_modules/",
+        "<rootDir>/coverage/",
+    ],
+    coveragePathIgnorePatterns: [
+        "<rootDir>/dist/",
+        "<rootDir>/node_modules/",
+        "<rootDir>/coverage/",
+    ]
+};
